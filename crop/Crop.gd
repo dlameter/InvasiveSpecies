c35f239f119f5e85c047164f55cf47540b6b3cc9
Dominik <dominik@lameter.com>
2023-07-08 18:36:55 -0500

    add plant that grows

--- crop/Crop.gd
+++ crop/Crop.gd
@@ -1,11 +1,36 @@
 class_name Crop extends Node2D
 
+enum Stage {
+	SEED,
+	SPROUT,
+	PLANT
+}
+
+var stage: Stage
+
+@onready var seed_sprite = $SeedSprite
+@onready var sprout_sprite = $SproutSprite
+@onready var plant_sprite = $PlantSprite 
+
+@onready var sprout_timer = $SproutTimer
+@onready var plant_timer = $PlantTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	stage = Stage.SEED
+	handle_stage_visuals()
+	sprout_timer.start()
 
+func transition_to_sprout():
+	stage = Stage.SPROUT
+	handle_stage_visuals()
+	plant_timer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func transition_to_plant():
+	stage = Stage.PLANT
+	handle_stage_visuals()
+
+func handle_stage_visuals():
+	seed_sprite.visible = stage == Stage.SEED
+	sprout_sprite.visible = stage == Stage.SPROUT
+	plant_sprite.visible = stage == Stage.PLANT
