d80c2875af4804442cf299df0d88df62c9842f5c
Dominik <dominik@lameter.com>
2023-07-08 18:04:29 -0500

    misc changes idk

---
+++ crop/cropPlot.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var crop: Crop = null
+
+func _ready():
+	set_crop(crop)
+
+func get_crop() -> Crop:
+	return crop
+
+func set_crop(new_crop: Crop):
+	var old_crop = crop
+	
+	if new_crop:
+		crop.global_position = global_position
+		add_child(crop, true)
+	
+	crop = new_crop
+	
