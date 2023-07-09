d7c26879233e257585ef74d4caab43ed78f3ae9d
JLameter <jonathan@lameter.com>
2023-07-09 11:11:41 -0500

    Whoops

---
+++ crop/dandelion_crop.gd
@@ -0,0 +1,20 @@
+extends Crop
+
+var puff = preload("res://crop/dandelion_puff.tscn")
+
+func dandyBurst(_unused):
+	for i in range(randi() % 3 + 2):
+		var puffButActually = puff.instantiate()
+		puffButActually.velocity = Vector2(randi() % 100 - 50,randi() % 100 - 50)
+		puffButActually.lifetime = 2
+		#puffButActually.owner_id = null
+		puffButActually.position = position
+		get_parent().add_child(puffButActually)
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	sprout_delay = 5
+	plant_delay = 5
+	super._ready()
+
