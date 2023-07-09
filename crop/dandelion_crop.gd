bd5702549ad8eb992475ffa4f3b0cf1293090e0b
Dominik <dominik@lameter.com>
2023-07-09 11:23:14 -0500

    sync puffs

--- crop/dandelion_crop.gd
+++ crop/dandelion_crop.gd
@@ -3,13 +3,16 @@ extends Crop
 var puff = preload("res://crop/dandelion_puff.tscn")
 
 func dandyBurst(_unused):
+	if not is_multiplayer_authority():
+		return;
+
 	for i in range(randi() % 3 + 2):
 		var puffButActually = puff.instantiate()
 		puffButActually.velocity = Vector2(randi() % 100 - 50,randi() % 100 - 50)
 		puffButActually.lifetime = 2
 		#puffButActually.owner_id = null
 		puffButActually.position = position
-		get_parent().add_child(puffButActually)
+		get_parent().add_child(puffButActually, true)
 	
 
 # Called when the node enters the scene tree for the first time.
