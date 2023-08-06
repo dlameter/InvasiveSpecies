859ca063bd19db573a9dc9101ed75a717def4b0c
Dominik <dominik@lameter.com>
2023-08-05 22:39:20 -0500

    fix a bunch of scaling issues

--- crop/ItemCrop.gd
+++ crop/ItemCrop.gd
@@ -4,11 +4,14 @@ extends Crop
 func _ready():
 	sprout_delay = 10
 	plant_delay = 10
-	fully_grown.connect(spawn_itembox)
+	
+	if is_multiplayer_authority():
+		fully_grown.connect(spawn_itembox)
+	
 	super._ready()
 
 
-func spawn_itembox():
+func spawn_itembox(_garbage):
 	var itembox = load("res://item/ItemBox.tscn").instantiate()
 	get_parent().add_child(itembox)
 	itembox.position = position
