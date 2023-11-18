2d5e51f8a6381bb90bb66ca93ea6de6df2097960
Dominik <dominik@lameter.com>
2023-11-17 21:49:15 -0600

    add gust animation and tweak distance from player that gust starts

--- crop/effect/BokChoyGust.gd
+++ crop/effect/BokChoyGust.gd
@@ -1,16 +1,31 @@
 extends Area2D
 
 
-func _ready():
+var pointing_direction: Vector2
+var gust_power: float = 500
+
+
+func _enter_tree():
+	$AnimationPlayer.play("push")
 	if is_multiplayer_authority():
 		$Timer.timeout.connect(queue_free)
+
+
+func _ready():
+	if is_multiplayer_authority():
 		body_entered.connect(handle_enter)
+
+
+func point_in(dir: Vector2):
+	global_rotation = dir.angle()
+	pointing_direction = dir.normalized()
 
 
 func handle_enter(node: PhysicsBody2D):
 	if node is CharacterBody2D:
 		node.velocity = 200 * Vector2(0, 1)
-	pass
+	elif node is RigidBody2D:
+		node.apply_impulse(pointing_direction * gust_power)
 
 
 func handle_character_body_enter(node: CharacterBody2D):
--- crop/item/bokchoy/BokChoyItem.gd
+++ crop/item/bokchoy/BokChoyItem.gd
@@ -2,11 +2,14 @@ extends CropItem
 
 const gust: PackedScene = preload("res://crop/effect/BokChoyGust.tscn")
 
+@onready var gust_location = $GustLocation 
+
 func throw(player: Player, dir: Vector2):
+	look_at(player.input.mouse_pos)
 	var gust_but_actually = gust.instantiate()
 	player.spawn_location.add_child(gust_but_actually, true)
-	gust_but_actually.position = global_position
-	gust_but_actually.global_rotation = dir.angle()
+	gust_but_actually.position = gust_location.global_position
+	gust_but_actually.point_in(dir)
 
 
 func hold(_player: Player):
